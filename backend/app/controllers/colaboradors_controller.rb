b4407dca1211e0f0fd9856fc81b107b237c67aa0
leonardo <omgleo@live.com>
2025-05-03 21:41:14 -0300

    adicionando controllers e rotas ,colaborador e  contrato colaboradors

---
+++ backend/app/controllers/colaboradors_controller.rb
@@ -0,0 +1,50 @@
+class ColaboradorsController < ApplicationController
+  before_action :set_colaborador, only: %i[ show update destroy ]
+
+  # GET /colaboradors
+  def index
+    @colaboradors = Colaborador.all
+
+    render json: @colaboradors
+  end
+
+  # GET /colaboradors/1
+  def show
+    render json: @colaborador
+  end
+
+  # POST /colaboradors
+  def create
+    @colaborador = Colaborador.new(colaborador_params)
+
+    if @colaborador.save
+      render json: @colaborador, status: :created, location: @colaborador
+    else
+      render json: @colaborador.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /colaboradors/1
+  def update
+    if @colaborador.update(colaborador_params)
+      render json: @colaborador
+    else
+      render json: @colaborador.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /colaboradors/1
+  def destroy
+    @colaborador.destroy!
+  end
+
+  private
+    
+    def set_colaborador
+      @colaborador = Colaborador.find(params[:id])
+    end
+
+    def colaborador_params
+      params.require(:colaborador).permit(:nome, :cpf, :cbo)
+    end
+end
