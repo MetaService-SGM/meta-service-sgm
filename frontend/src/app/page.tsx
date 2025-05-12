d9e69d8a2d0ccfca96b34a39d5d2ba03612944d2
Catarina Guimarães <catarina.guimaraes15@gmail.com>
2025-05-12 16:42:31 -0300

    feat: Instala a lib shadcn/ui

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,16 @@
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button";
+import { Label } from "@radix-ui/react-label";
+
 export default function Home() {
   return (
     <div
       style={{ fontFamily: "var(--font-nunito)" }}
       className="text-2xl text-black"
     >
-      Olá, esta é a fonte Nunito!
+      <Label>Nome</Label>
+      <Input placeholder="Digite aqui"/>
+      <Button className="cursor-pointer">Enviar</Button>
     </div>
   );
 }
