8c71b02484775fa958cb55a57298f12eab215e6a
MrGalva07 <lucas.galvao0707@gmail.com>
2025-05-13 19:29:30 -0300

    feat:TableComponent

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,13 @@
+import AlertTable from "./components/ui/tables/AlertTable";
+import { mockAlerts, mockCompanies, mockproducts, mockServiceOrder } from "./components/ui/tables/mocks";
+import ServiceOrderTable from "./components/ui/tables/ServiceOrderTable";
+import StockTable from "./components/ui/tables/StockTable";
+import TableCompanies from "./components/ui/tables/TableCompanies";
+
 export default function Home() {
   return (
-    <div
-      style={{ fontFamily: "var(--font-nunito)" }}
-      className="text-2xl text-black"
-    >
-      Olá, esta é a fonte Nunito!
+    <div>
+    
     </div>
   );
 }
