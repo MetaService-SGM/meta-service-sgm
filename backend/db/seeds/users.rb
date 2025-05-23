8806bd280b8d576a55e68e6039079fc26c01d443
FabioBezerradeAndrade <75230523+FabioBezerradeAndrade@users.noreply.github.com>
2025-05-23 01:02:29 -0300

    adjuste user and colaborador models to check cpf validate

--- backend/db/seeds/users.rb
+++ backend/db/seeds/users.rb
@@ -6,11 +6,11 @@ User.destroy_all
 puts "🌱 Criando usuários..."
 
 users = [
-  { name: "Admin 1", email: "admin1@example.com", password: "password123", role: :admin, cpf: "11111111111", tipo_contrato: "CLT", ativo: true },
-  { name: "Gerente 1", email: "gerente1@example.com", password: "password123", role: :gerente, cpf: "22222222222", tipo_contrato: "PJ", ativo: true },
-  { name: "Contratado 1", email: "contratado1@example.com", password: "password123", role: :contratado, cpf: "33333333333", tipo_contrato: "Estágio", ativo: false },
-  { name: "Contratado 2", email: "contratado2@example.com", password: "password123", role: :contratado, cpf: "44444444444", tipo_contrato: "CLT", ativo: true },
-  { name: "Gerente 2", email: "gerente2@example.com", password: "password123", role: :gerente, cpf: "55555555555", tipo_contrato: "PJ", ativo: false }
+  { name: "Admin 1", email: "admin1@example.com", password: "password123", role: :admin, cpf: CPF.generate, tipo_contrato: "CLT", ativo: true },
+  { name: "Gerente 1", email: "gerente1@example.com", password: "password123", role: :gerente, cpf: CPF.generate, tipo_contrato: "PJ", ativo: true },
+  { name: "Contratado 1", email: "contratado1@example.com", password: "password123", role: :contratado, cpf: CPF.generate, tipo_contrato: "Estágio", ativo: false },
+  { name: "Contratado 2", email: "contratado2@example.com", password: "password123", role: :contratado, cpf: CPF.generate, tipo_contrato: "CLT", ativo: true },
+  { name: "Gerente 2", email: "gerente2@example.com", password: "password123", role: :gerente, cpf: CPF.generate, tipo_contrato: "PJ", ativo: false }
 ]
 
 users.each do |attrs|
