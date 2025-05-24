7cff2b06b7ce341b948fa506374ef258e0fa6ea3
FabioBezerradeAndrade <75230523+FabioBezerradeAndrade@users.noreply.github.com>
2025-05-24 11:16:14 -0300

    create empresa model

---
+++ backend/app/policies/empresa_policy.rb
@@ -0,0 +1,27 @@
+class EmpresaPolicy < ApplicationPolicy
+  def index?
+    user.present?
+  end
+
+  def show?
+    user.present?
+  end
+
+  def create?
+    user.admin?
+  end
+
+  def update?
+    user.admin?
+  end
+
+  def destroy?
+    user.admin?
+  end
+
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+end
