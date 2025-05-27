1413b76949eba19d6d26ed1cbcc78a61184c6033
Mateus Fernando <mateusfss368@gmail.com>
2025-05-27 05:00:43 -0300

    alterações minimas

--- frontend/src/app/bemVindo/index.tsx
+++ frontend/src/app/bemVindo/index.tsx
@@ -5,19 +5,24 @@ export default function BemvindoPage() {
   const dataFormatada = formatDatePTBR();
 
   return (
+    // <PageLayout fixedHeight="1300px">
     <PageLayout>
-      <div className="flex-1 flex flex-col justify-center items-center text-center space-y-4">
-        <h1>
-          Bem-Vindo(a) ao <b>SGM!</b>
-        </h1>
-        <div>
-          <p className="max-w-lg">
-            Utilize a barra de menus para ter acesso às funcionalidades do
-            sistema.
-          </p>
-        </div>
-        <div>
-          <span className="text-sm text-gray-600">{dataFormatada}</span>
+
+      <div className="h-full flex items-center justify-center">
+
+        <div className="flex flex-col items-center text-center space-y-4">
+          <h1>
+            Bem-Vindo(a) ao <b>SGM!</b>
+          </h1>
+          <div>
+            <p className="max-w-lg">
+              Utilize a barra de menus para ter acesso às funcionalidades do
+              sistema.
+            </p>
+          </div>
+          <div>
+            <span className="text-sm text-gray-600">{dataFormatada}</span>
+          </div>
         </div>
       </div>
     </PageLayout>
