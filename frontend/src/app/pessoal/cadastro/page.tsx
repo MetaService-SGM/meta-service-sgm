e116c5b68b53415b967c16b597b40f42bb26ed15
Catarina Guimarães <catarina.guimaraes15@gmail.com>
2025-05-27 00:15:56 -0300

    feat: Implementa os componentes inputFileImage e InputDocument

--- frontend/src/app/pessoal/cadastro/page.tsx
+++ frontend/src/app/pessoal/cadastro/page.tsx
@@ -1,12 +1,18 @@
 "use client";
 
 import { DatePicker } from "@/components/ui/date-picker";
+import { InputDocument } from "@/components/ui/input/inputDocument";
+import { InputFileImage } from "@/components/ui/input/inputFileImage";
 import React from "react";
 
 export default function EmployeeRegistration() {
   return (
-    <>
+    <div className="h-screen flex flex-col justify-center items-center ">
       <DatePicker />
-    </>
+      <InputFileImage/>
+      <InputDocument />
+    </div>
   );
 }
+
+
