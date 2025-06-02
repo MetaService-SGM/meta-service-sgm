4d06f8086e1985fe6063288f96df5e665b55892a
Mathias <mathiasernandes02@hotmail.com>
2025-06-02 20:55:08 -0300

    acrescentando active_storage na tabela certificado

--- backend/app/serializers/certificacao_serializer.rb
+++ backend/app/serializers/certificacao_serializer.rb
@@ -9,6 +9,7 @@ class CertificacaoSerializer
       colaborador_nome: certificacao.colaborador.nome,
       cargo_id: certificacao.cargo_id,
       cargo_nome: certificacao.cargo.nome
+      pdf_url: certificacao.pdf.attached? ? Rails.application.routes.url_helpers.rails_blob_url(certificacao.pdf, only_path: true) : nil
     }
   end
 end
